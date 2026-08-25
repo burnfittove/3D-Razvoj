6aa9e5527bbd66077ea1c46cb19dfb5af47e5328
Mia <ian.dobrincic@gmail.com>
2026-08-25 21:36:00 +0200

    added a cursor manager

--- Assets/Code/Enemies/ReaperController.cs
+++ Assets/Code/Enemies/ReaperController.cs
@@ -1,13 +1,27 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ReaperController : MonoBehaviour
 {
     [SerializeField] private float damage;
     public float Damage { get; private set; }
+    private NavMeshAgent agent;
 
     private void Awake()
     {
         Damage = damage;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        GameEventManager.instance.sceneEvents.OnTransitionStarted += DisableAgent;
+    }
+
+    private void DisableAgent()
+    {
+        agent.speed = 0;
+        agent.angularSpeed = 0;
     }
 }
