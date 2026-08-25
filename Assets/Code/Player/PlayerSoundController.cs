364886c5d4d08efee6e3cc7968bf02157bb221bc
Mia <ian.dobrincic@gmail.com>
2026-08-25 14:25:30 +0200

    pitch and volume changes

--- Assets/Code/Player/PlayerSoundController.cs
+++ Assets/Code/Player/PlayerSoundController.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class PlayerSoundController : MonoBehaviour
 {
     public AudioSource _audioSource;
+    public Vector2 pitchRangeInclusive = new(0.2f, 0.6f);
     
     private void PlayFootstepSound()
     {
-        _audioSource.pitch = Random.Range(0.4f, 0.8f);
+        _audioSource.pitch = Random.Range(pitchRangeInclusive.x, pitchRangeInclusive.y);
         _audioSource.Play();
     }
 }
