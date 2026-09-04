a4d57c5bfb970de9c481fbf39eb0404e4eabd702
Mia <ian.dobrincic@gmail.com>
2026-09-04 17:45:05 +0200

    added an exit

--- Assets/Code/UnlockMainDoor.cs
+++ Assets/Code/UnlockMainDoor.cs
@@ -1,10 +1,30 @@
-using System;
 using UnityEngine;
 
 public class UnlockMainDoor : MonoBehaviour
 {
+    private DisplayTextOnContact displayTextComponent;
+    private TouchSceneChange sceneChangeComponent;
+
+    private void Awake()
+    {
+        TryGetComponent(out displayTextComponent);
+        TryGetComponent(out sceneChangeComponent);
+    }
+
     private void Start()
     {
+        if (SpiritManager.instance.AllSpiritsCollected)
+        {
+            SetDoorState(true);
+            return;
+        }
         
+        SetDoorState(false);
+    }
+    
+    private void SetDoorState(bool isLocked)
+    {
+        displayTextComponent.enabled = isLocked;
+        sceneChangeComponent.enabled = !isLocked;
     }
 }
